3fc1a7ccbe8b1fa76c68d531661ffd6171481492
WalterQuaestio <145466767+WalterQuaestio@users.noreply.github.com>
2023-12-21 21:31:53 +0700

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,96 @@
 
 def sum arr
   # YOUR CODE HERE
+  unless arr.all? { |elem| elem.is_a?(Integer) }
+    puts "Please enter an array of numbers."
+    return
+  end
+  ()
+  
+  if arr.empty?
+    sum = 0
+  else
+    i = 0
+    sum = 0
+
+    while i <= (arr.length - 1) do
+      sum += arr[i]
+      i += 1
+    end
+  end
+  sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  unless arr.all? {|elem| elem.is_a?(Integer)}
+    puts "Please enter an arrya of number."
+  end
+  
+  if arr.empty? 
+    sum = 0
+    
+  elsif arr.length == 1 
+    sum = arr[0]
+    
+  else
+    sorted_arr = arr.sort.reverse
+    sum = sorted_arr[0] + sorted_arr[1]
+    
+  end
+  
+  sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  pairs = arr.combination(2)
+
+  pairs.any? { |pair| pair.sum == n }
 end
+
+
+
+
+
+
+
+
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant? (str)
+  /^[b-df-hj-np-tv-z]/i.match?(str)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /^[01]*00$/.match?(str)
 end
+
+
+
+
+
+
+
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'Invalid ISBN number' if isbn.empty?
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', @price)
+  end
 end
